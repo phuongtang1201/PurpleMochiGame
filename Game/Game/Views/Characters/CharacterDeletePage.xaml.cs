792e8934521c72761b09e1802842723de2d4b4f6
Erika <erikasolsen@yahoo.com>
2022-02-08 02:51:39 -0800

    Added Item box methods to CharacterDeletePage.xaml.cs.

--- Game/Game/Views/Characters/CharacterDeletePage.xaml.cs
+++ Game/Game/Views/Characters/CharacterDeletePage.xaml.cs
@@ -6,6 +6,7 @@ using Xamarin.Forms.Xaml;
 
 using Game.ViewModels;
 using Game.Models;
+using System.Linq;
 
 namespace Game.Views
 {
@@ -17,7 +18,7 @@ namespace Game.Views
     public partial class CharacterDeletePage : ContentPage
     {
         // View Model for Character
-        readonly GenericViewModel<CharacterModel> viewModel;
+        readonly GenericViewModel<CharacterModel> ViewModel;
 
         // Empty Constructor for UTs
         public CharacterDeletePage(bool UnitTest) { }
@@ -27,9 +28,117 @@ namespace Game.Views
         {
             InitializeComponent();
 
-            BindingContext = this.viewModel = data;
+            BindingContext = this.ViewModel = data;
 
-            this.viewModel.Title = "Delete " + data.Title;
+            this.ViewModel.Title = "Delete " + data.Title;
+
+            AddItemsToDisplay();
+        }
+
+        /// <summary>
+        /// Show the Items the Character has
+        /// </summary>
+        public void AddItemsToDisplay()
+        {
+            var FlexList = ItemBox.Children.ToList();
+            foreach (var data in FlexList)
+            {
+                _ = ItemBox.Children.Remove(data);
+            }
+
+            ItemBox.Children.Add(GetItemToDisplay(ItemLocationEnum.Head));
+            ItemBox.Children.Add(GetItemToDisplay(ItemLocationEnum.Necklass));
+            ItemBox.Children.Add(GetItemToDisplay(ItemLocationEnum.PrimaryHand));
+            ItemBox.Children.Add(GetItemToDisplay(ItemLocationEnum.OffHand));
+            ItemBox.Children.Add(GetItemToDisplay(ItemLocationEnum.RightFinger));
+            ItemBox.Children.Add(GetItemToDisplay(ItemLocationEnum.LeftFinger));
+            ItemBox.Children.Add(GetItemToDisplay(ItemLocationEnum.Feet));
+        }
+
+        /// <summary>
+        /// Look up the Item to Display
+        /// </summary>
+        /// <param name="location"></param>
+        /// <returns></returns>
+        public StackLayout GetItemToDisplay(ItemLocationEnum location)
+        {
+            // Defualt Image is the Plus
+            var ImageSource = "icon_cancel.png";
+            var ClickableButton = true;
+
+            var data = ViewModel.Data.GetItemByLocation(location);
+            if (data == null)
+            {
+                // Show the Default Icon for the Location
+                data = new ItemModel { Location = location, ImageURI = ImageSource };
+
+                // Turn off click action
+                ClickableButton = false;
+            }
+
+            // Hookup the Image Button to show the Item picture
+            var ItemButton = new ImageButton
+            {
+                Style = (Style)Application.Current.Resources["ImageMediumStyle"],
+                Source = data.ImageURI
+            };
+
+            if (ClickableButton)
+            {
+                // Add a event to the user can click the item and see more
+                ItemButton.Clicked += (sender, args) => ShowPopup(data);
+            }
+
+            // Add the Display Text for the item
+            var ItemLabel = new Label
+            {
+                Text = location.ToMessage(),
+                Style = (Style)Application.Current.Resources["ValueStyleMicro"],
+                HorizontalOptions = LayoutOptions.Center,
+                HorizontalTextAlignment = TextAlignment.Center
+            };
+
+            // Put the Image Button and Text inside a layout
+            var ItemStack = new StackLayout
+            {
+                Margin = 3,
+                Style = (Style)Application.Current.Resources["ItemImageLabelBox"],
+                HorizontalOptions = LayoutOptions.Center,
+                Children = {
+                    ItemButton,
+                    ItemLabel
+                },
+            };
+
+            return ItemStack;
+        }
+
+        /// <summary>
+        /// Show the Popup for the Item
+        /// </summary>
+        /// <param name="data"></param>
+        /// <returns></returns>
+        public bool ShowPopup(ItemModel data)
+        {
+
+
+            //// Close the popup after 3 seconds
+            //Device.StartTimer(TimeSpan.FromSeconds(3), () => { 
+            //        PopupLoadingView.IsVisible = false;
+            //        return true; }); 
+
+            return false;
+        }
+
+        /// <summary>
+        /// When the user clicks the close in the Popup
+        /// hide the view
+        /// show the scroll view
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        public void ClosePopup_Clicked(object sender, EventArgs e)
+        {
         }
 
         /// <summary>
@@ -39,7 +148,7 @@ namespace Game.Views
         /// <param name="e"></param>
         public async void Delete_Clicked(object sender, EventArgs e)
         {
-            MessagingCenter.Send(this, "Delete", viewModel.Data);
+            MessagingCenter.Send(this, "Delete", ViewModel.Data);
             _ = await Navigation.PopModalAsync();
         }
 
