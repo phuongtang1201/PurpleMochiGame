c7ee0747c543c1bc110c8571762e94c9e5f513b7
Erika <erikasolsen@yahoo.com>
2022-02-13 03:23:34 -0800

    Changed Characters to Chefs in CharacterIndexViewModel.cs.

--- Game/Game/ViewModels/CharacterIndexViewModel.cs
+++ Game/Game/ViewModels/CharacterIndexViewModel.cs
@@ -53,7 +53,7 @@ namespace Game.ViewModels
         /// </summary>
         public CharacterIndexViewModel()
         {
-            Title = "Characters";
+            Title = "Chefs";
 
             #region Messages
 
