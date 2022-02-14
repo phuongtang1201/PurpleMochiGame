7e1196a1a80de223d3244ee843863066bdeeb61b
Erika <erikasolsen@yahoo.com>
2022-02-14 00:36:47 -0800

    Changed string test in RandomPlayerHelper_GetRandomCharacter_InValid_Empty_CharacterList_Should_Return_New for RandomPlayerHelperTests.cs.

--- UnitTests/GameRules/RandomPlayerHelperTests.cs
+++ UnitTests/GameRules/RandomPlayerHelperTests.cs
@@ -244,7 +244,7 @@ namespace UnitTests.Helpers
             // Reset
 
             // Assert
-            Assert.AreEqual(true, result.Name.Contains("Character Name"));
+            Assert.AreEqual(true, result.Name.Contains("Chef Name"));
         }
 
         [Test]
