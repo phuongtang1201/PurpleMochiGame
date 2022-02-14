2c24f470845c887b68e008a240df09dbda62fb9d
Erika <erikasolsen@yahoo.com>
2022-02-13 23:34:40 -0800

    Changed test value in DefaultData_Valid_LoadData_Monster_Should_Pass in DefaultDataTests.cs.

--- UnitTests/GameRules/DefaultDataTests.cs
+++ UnitTests/GameRules/DefaultDataTests.cs
@@ -49,7 +49,7 @@ namespace UnitTests.GameRules
             // Reset
 
             // Assert
-            Assert.AreEqual(8, result.Count);
+            Assert.AreEqual(7, result.Count);
         }
 
 
