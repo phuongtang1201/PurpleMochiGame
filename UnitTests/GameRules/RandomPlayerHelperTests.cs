006ce462b6f11c4c6f804d43972a992773b76632
Erika <erikasolsen@yahoo.com>
2022-02-14 00:34:54 -0800

    Changed string test in RandomPlayerHelper_GetMonsterDescription_2_Should_Return_2 for RandomPlayerHelperTests.cs.

--- UnitTests/GameRules/RandomPlayerHelperTests.cs
+++ UnitTests/GameRules/RandomPlayerHelperTests.cs
@@ -94,7 +94,7 @@ namespace UnitTests.Helpers
             _ = DiceHelper.DisableForcedRolls();
 
             // Assert
-            Assert.AreEqual("the Elf hater", result);
+            Assert.AreEqual("Rotten", result);
         }
 
         [Test]
