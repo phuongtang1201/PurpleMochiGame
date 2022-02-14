eef06137e78d1713f05a0b12ead08204210550e8
phuongtang1201 <mtang1@seattleu.edu>
2022-02-13 19:15:41 -0800

    Changed in Image_OnPickerValueChanged so that when source changes, the large image changes: MonsterUpdatePage.xaml.cs

--- Game/Game/Views/Monsters/MonsterUpdatePage.xaml.cs
+++ Game/Game/Views/Monsters/MonsterUpdatePage.xaml.cs
@@ -153,7 +153,7 @@ namespace Game.Views
         public void Image_OnPickerValueChanged(object sender, EventArgs e)
         {
             ViewModel.Data.ImageURI = ImagePicker.SelectedItem.ToString();
-
+            LargeImage.Source = ViewModel.Data.ImageURI;
         }
 
 
