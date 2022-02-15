e4cd6b9feca352742902bc5d7a37289e6c26a76b
phuongtang1201 <mtang1@seattleu.edu>
2022-02-14 21:23:19 -0800

    Removed commented code in MonsterUpdatePage.xaml.cs

--- Game/Game/Views/Monsters/MonsterUpdatePage.xaml.cs
+++ Game/Game/Views/Monsters/MonsterUpdatePage.xaml.cs
@@ -187,30 +187,5 @@ namespace Game.Views
             SpeedValue.Text = string.Format("{0}", Convert.ToInt32(e.NewValue));
         }
 
-        ///// <summary>
-        ///// Randomize the Monster, keep the level the same
-        ///// </summary>
-        ///// <returns></returns>
-        //public bool RandomizeMonster()
-        //{
-        //    // Randomize Name
-        //    ViewModel.Data.Name = RandomPlayerHelper.GetMonsterName();
-        //    ViewModel.Data.Description = RandomPlayerHelper.GetMonsterDescription();
-
-        //    // Randomize the Attributes
-        //    ViewModel.Data.Attack = RandomPlayerHelper.GetAbilityValue();
-        //    ViewModel.Data.Speed = RandomPlayerHelper.GetAbilityValue();
-        //    ViewModel.Data.Defense = RandomPlayerHelper.GetAbilityValue();
-
-        //    ViewModel.Data.Difficulty = RandomPlayerHelper.GetMonsterDifficultyValue();
-
-        //    ViewModel.Data.ImageURI = RandomPlayerHelper.GetMonsterImage();
-
-        //    ViewModel.Data.UniqueItem = RandomPlayerHelper.GetMonsterUniqueItem();
-
-        //    _ = UpdatePageBindingContext();
-
-        //    return true;
-        //}
     }
 }
