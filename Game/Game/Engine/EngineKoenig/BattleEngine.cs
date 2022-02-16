c5b00da5163f0c83d04618e1f89bf57c8fb6d2be
Veronica-Carpenter <veronica1north@gmail.com>
2022-02-15 22:33:52 -0800

    Undid changes in BattleEngine.cs

--- Game/Game/Engine/EngineKoenig/BattleEngine.cs
+++ Game/Game/Engine/EngineKoenig/BattleEngine.cs
@@ -2,7 +2,6 @@
 using Game.Engine.EngineInterfaces;
 using Game.Engine.EngineModels;
 using Game.Models;
-using Game.ViewModels;
 
 namespace Game.Engine.EngineKoenig
 {
@@ -52,9 +51,6 @@ namespace Game.Engine.EngineKoenig
             {
                 AutoBattle = isAutoBattle
             };
-
-            // Add Score to track characters/monsters/items
-            ScoreIndexViewModel.Instance.Dataset.Add(EngineSettings.BattleScore);
 
             BattleRunning = true;
 
