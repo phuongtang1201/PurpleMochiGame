a350c422e1af61b40409145a30abab23f9b7452b
phuongtang1201 <mtang1@seattleu.edu>
2022-02-16 13:41:52 -0800

    Added UT ItemUpdatePage_Entry_CheckNotEmpty_Null_Name in ItemUdpatePageTests.cs

--- UnitTests/Views/Items/ItemUpdatePageTests.cs
+++ UnitTests/Views/Items/ItemUpdatePageTests.cs
@@ -173,5 +173,21 @@ namespace UnitTests.Views
             // Assert
             Assert.IsTrue(true); // Got to here, so it happened...
         }
+
+        [Test]
+        public void ItemUpdatePage_Entry_CheckNotEmpty_Null_Name_Should_Pass()
+        {
+            // Arrange
+            page.ViewModel.Data.Name = null;
+
+            // Act
+            page.Entry_CheckNotEmpty(null, null);
+
+
+            // Reset
+
+            // Assert
+            Assert.IsTrue(true); // Got to here, so it happened...
+        }
     }
 }
