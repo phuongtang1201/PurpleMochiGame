db09722949532ddbbad40ed589f8fa4fb014e0b7
phuongtang1201 <mtang1@seattleu.edu>
2022-02-16 13:39:46 -0800

    Added UT ItemCreatePage_Entry_CheckNotEmpty_Null_Name in ItemCreatePageTests.cs

--- UnitTests/Views/Items/ItemCreatePageTests.cs
+++ UnitTests/Views/Items/ItemCreatePageTests.cs
@@ -221,5 +221,21 @@ namespace UnitTests.Views
             Assert.IsTrue(true); // Got to here, so it happened...
         }
 
+        [Test]
+        public void ItemCreatePage_Entry_CheckNotEmpty_Null_Name_Should_Pass()
+        {
+            // Arrange
+            page.ViewModel.Data.Name = null;
+
+            // Act
+            page.Entry_CheckNotEmpty(null, null);
+
+
+            // Reset
+
+            // Assert
+            Assert.IsTrue(true); // Got to here, so it happened...
+        }
+
     }
 }
