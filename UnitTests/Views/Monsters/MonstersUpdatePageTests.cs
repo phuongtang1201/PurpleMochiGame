44edade77ff5e4c8d2ba764e4ea102276dd147f7
phuongtang1201 <mtang1@seattleu.edu>
2022-02-16 13:36:43 -0800

    Added UT MonsterUpdatePage_Entry_CheckNotEmpty_Null_Description_Should_Pass in MonsterUpdatePageTests.cs

--- UnitTests/Views/Monsters/MonstersUpdatePageTests.cs
+++ UnitTests/Views/Monsters/MonstersUpdatePageTests.cs
@@ -164,5 +164,21 @@ namespace UnitTests.Views
             // Assert
             Assert.IsTrue(true); // Got to here, so it happened...
         }
+
+        [Test]
+        public void MonsterUpdatePage_Entry_CheckNotEmpty_Null_Description_Should_Pass()
+        {
+            // Arrange
+            page.ViewModel.Data.Description = null;
+
+            // Act
+            page.Entry_CheckNotEmpty(null, null);
+
+
+            // Reset
+
+            // Assert
+            Assert.IsTrue(true); // Got to here, so it happened...
+        }
     }
 }
