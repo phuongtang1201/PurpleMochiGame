643e9b9b5adae1cc9dcd54dfc645c99a56bcba84
Veronica-Carpenter <veronica1north@gmail.com>
2022-02-20 20:28:40 -0800

    Removed monsters killed value. Changes in ScorePage.xaml, ScorePage.xaml.cs

--- Game/Game/Views/Battle/ScorePage.xaml.cs
+++ Game/Game/Views/Battle/ScorePage.xaml.cs
@@ -47,7 +47,7 @@ namespace Game.Views
             }
 
             // Update Values in the UI
-            TotalKilled.Text = EngineViewModel.Engine.EngineSettings.BattleScore.MonsterModelDeathList.Count().ToString();
+            //TotalKilled.Text = EngineViewModel.Engine.EngineSettings.BattleScore.MonsterModelDeathList.Count().ToString();
             TotalCollected.Text = EngineViewModel.Engine.EngineSettings.BattleScore.ItemModelDropList.Count().ToString();
             TotalScore.Text = EngineViewModel.Engine.EngineSettings.BattleScore.ExperienceGainedTotal.ToString();
         }
