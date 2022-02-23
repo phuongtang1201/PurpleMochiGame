5bae5c6027a1a932a9264539d343386332e16beb
phuongtang1201 <mtang1@seattleu.edu>
2022-02-23 09:36:05 -0800

    Changed namespace in BattleHomePage.xaml.cs

--- Game/Game/Views/Battle/BattleHomePage.xaml.cs
+++ Game/Game/Views/Battle/BattleHomePage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Game.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,7 +8,7 @@ using System.Threading.Tasks;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
-namespace Game.Views.Battle
+namespace Game.Views
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class BattleHomePage : ContentPage
