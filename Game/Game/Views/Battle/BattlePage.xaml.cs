06d138dda3d215ac2555db98189c6fd76da5eaf5
Erika <erikasolsen@yahoo.com>
2022-02-24 02:00:24 -0800

    Changed exit button to navigate to Game page in BattlePage.xaml.cs. Commented out button in BattlePage.xaml.

--- Game/Game/Views/Battle/BattlePage.xaml.cs
+++ Game/Game/Views/Battle/BattlePage.xaml.cs
@@ -804,7 +804,8 @@ namespace Game.Views
         /// <param name="e"></param>
         public async void ExitButton_Clicked(object sender, EventArgs e)
         {
-            _ = await Navigation.PopModalAsync();
+            //_ = await Navigation.PopModalAsync();
+            await Navigation.PushModalAsync(new NavigationPage (new GamePage()));
         }
 
         /// <summary>
