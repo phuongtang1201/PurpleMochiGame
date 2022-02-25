2b8844c042acc53d47ca02d90366d13d47ac97d8
Erika <erikasolsen@yahoo.com>
2022-02-24 23:12:57 -0800

    Removed default image from AddItemsPage.xaml.cs.

--- Game/Game/Views/Battle/AddItemsPage.xaml.cs
+++ Game/Game/Views/Battle/AddItemsPage.xaml.cs
@@ -117,11 +117,6 @@ namespace Game.Views
         /// <param name="e"></param>
         public async void Save_Clicked(object sender, EventArgs e)
         {
-            // If the image in the data box is empty, use the default one..
-            if (string.IsNullOrEmpty(ViewModel.Data.ImageURI))
-            {
-                ViewModel.Data.ImageURI = new CharacterModel().ImageURI;
-            }
             _ = await Navigation.PopModalAsync();
         }
 
