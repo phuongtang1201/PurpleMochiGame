030833e9969dd51fd3fcb4d64ff5dc33ad652cf4
phuongtang1201 <mtang1@seattleu.edu>
2022-03-09 22:49:11 -0800

    Added UT BattleSettingsPage_AllowBossMonsters_Toggled_Default in BattleSettingsPageTests.cs

--- UnitTests/Views/Battle/BattleSettingsPageTests.cs
+++ UnitTests/Views/Battle/BattleSettingsPageTests.cs
@@ -105,6 +105,25 @@ namespace UnitTests.Views
             // Assert
             Assert.IsTrue(!current); // Got to here, so it happened...
         }
+        [Test]
+        public void BattleSettingsPage_AllowBossMonsters_Toggled_Default_Should_Pass()
+        {
+            // Arrange
+
+            var control = (Switch)page.FindByName("AllowBossMonstersSwitch");
+            var current = control.IsToggled;
+
+            ToggledEventArgs args = new ToggledEventArgs(current);
+
+            // Act
+            page.AllowBossMonsters_Toggled(null, args);
+
+            // Reset
+
+            // Assert
+            Assert.IsTrue(!current); // Got to here, so it happened...
+        }
+        
 
         [Test]
         public void BattleSettingsPage_AllowCriticalMiss_Toggled_Default_Should_Pass()
