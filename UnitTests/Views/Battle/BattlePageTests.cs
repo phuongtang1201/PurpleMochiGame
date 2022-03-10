6f0bf7d846cc782740b5afd1d8da856a7c9368c7
phuongtang1201 <mtang1@seattleu.edu>
2022-03-09 21:31:07 -0800

    Added  BattlePage_FocusedAttackButton_Clicked_Default in BattlePageTests.cs

--- UnitTests/Views/Battle/BattlePageTests.cs
+++ UnitTests/Views/Battle/BattlePageTests.cs
@@ -92,6 +92,20 @@ namespace UnitTests.Views
         }
 
         [Test]
+        public void BattlePage_FocusedAttackButton_Clicked_Default_Should_Pass()
+        {
+            // Arrange
+
+            // Act
+            page.FocusedAttackButton_Clicked(null, null);
+
+            // Reset
+
+            // Assert
+            Assert.IsTrue(true); // Got to here, so it happened...
+        }
+
+        [Test]
         public void BattlePage_ShowScoreButton_Clicked_Default_Should_Pass()
         {
             // Arrange
