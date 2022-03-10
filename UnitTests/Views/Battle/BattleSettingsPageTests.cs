34b512ce77fdceb90d48111cd9b20f9c1960b581
phuongtang1201 <mtang1@seattleu.edu>
2022-03-09 23:02:38 -0800

    Added UT BattleSettingsPage_AllowZombieMonsters_Toggled_True_Default in BattleSettingsPageTests.cs

--- UnitTests/Views/Battle/BattleSettingsPageTests.cs
+++ UnitTests/Views/Battle/BattleSettingsPageTests.cs
@@ -167,6 +167,28 @@ namespace UnitTests.Views
         }
 
         [Test]
+        public void BattleSettingsPage_AllowZombieMonsters_Toggled_True_Default_Should_Pass()
+        {
+            // Arrange
+
+            var control = (Switch)page.FindByName("AllowZombieMonstersSwitch");
+            var current = control.IsToggled;
+
+            ToggledEventArgs args = new ToggledEventArgs(current);
+            page.AllowZombieMonsters_Toggled(null, args);
+
+            control.IsToggled = true;
+
+            // Act
+            page.AllowZombieMonsters_Toggled(null, args);
+
+            // Reset
+
+            // Assert
+            Assert.IsTrue(!current); // Got to here, so it happened...
+        }
+
+        [Test]
         public void BattleSettingsPage_AllowCriticalMiss_Toggled_Default_Should_Pass()
         {
             // Arrange
