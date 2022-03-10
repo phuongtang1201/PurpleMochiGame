e7aefd7b812dc19b544f14d2c6ba6e1fc9095c45
Veronica-Carpenter <veronica1north@gmail.com>
2022-03-09 20:16:56 -0800

    Changed assembly info in AssemblyInfo.cs

--- Game/Game.UWP/Properties/AssemblyInfo.cs
+++ Game/Game.UWP/Properties/AssemblyInfo.cs
@@ -4,13 +4,13 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Game.UWP")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Food Fight")]
+[assembly: AssemblyDescription("Cook for your life!")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Seattle U")]
 [assembly: AssemblyProduct("Game.UWP")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("FoodFight")]
 [assembly: AssemblyCulture("")]
 
 // Version information for an assembly consists of the following four values:
