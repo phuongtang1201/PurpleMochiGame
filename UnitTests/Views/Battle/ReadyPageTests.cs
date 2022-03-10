04884557d90c9f70594fb76ca46de98fab85a911
phuongtang1201 <mtang1@seattleu.edu>
2022-03-09 23:25:03 -0800

    Added UT AddItemsPage_Constructor_Default in ReadyPageTests.cs

--- UnitTests/Views/Battle/ReadyPageTests.cs
+++ UnitTests/Views/Battle/ReadyPageTests.cs
@@ -1,12 +1,52 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using NUnit.Framework;
 
-namespace UnitTests.Views.Battle
+using Game;
+using Game.Views;
+using Xamarin.Forms.Mocks;
+using Xamarin.Forms;
+using Game.Helpers;
+using Game.Models;
+
+namespace UnitTests.Views
 {
-    class ReadyPageTests
+    [TestFixture]
+    public class AddReadyPageTests
     {
+        App app;
+        AddReadyPageTests page;
+
+        [SetUp]
+        public void Setup()
+        {
+            // Initilize Xamarin Forms
+            MockForms.Init();
+
+            //This is your App.xaml and App.xaml.cs, which can have resources, etc.
+            app = new App();
+            Application.Current = app;
+
+            page = new AddReadyPageTests();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            Application.Current = null;
+        }
+
+        [Test]
+        public void AddItemsPage_Constructor_Default_Should_Pass()
+        {
+            // Arrange
+
+            // Act
+            var result = page;
+
+            // Reset
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+
     }
 }
