ec18dee2003250e00b25e673d6253ab996d3b68e
phuongtang1201 <mtang1@seattleu.edu>
2022-03-09 23:59:48 -0800

    Added UT  AddItemsPage_Cancel_Clicked_Default in AddItems:PageTests.cs

--- UnitTests/Views/Battle/AddItemsPageTests.cs
+++ UnitTests/Views/Battle/AddItemsPageTests.cs
@@ -116,5 +116,19 @@ namespace UnitTests.Views
             Assert.IsTrue(true); // Got to here, so it happened...
         }
 
+        [Test]
+        public void AddItemsPage_Cancel_Clicked_Default_Should_Pass()
+        {
+            // Arrange
+
+            // Act
+            page.Cancel_Clicked(null, null);
+
+            // Reset
+
+            // Assert
+            Assert.IsTrue(true); // Got to here, so it happened...
+        }
+
     }
 }
