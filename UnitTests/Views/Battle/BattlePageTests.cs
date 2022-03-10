92e1815749d42030980d823769d09a95c869018d
phuongtang1201 <mtang1@seattleu.edu>
2022-03-10 14:39:52 -0800

    Added UT BattlePage_HealButton_Clicked_Default in BattlePageTests.cs

--- UnitTests/Views/Battle/BattlePageTests.cs
+++ UnitTests/Views/Battle/BattlePageTests.cs
@@ -90,6 +90,19 @@ namespace UnitTests.Views
             // Assert
             Assert.IsTrue(true); // Got to here, so it happened...
         }
+        [Test]
+        public void BattlePage_HealButton_Clicked_Default_Should_Pass()
+        {
+            // Arrange
+
+            // Act
+            page.HealButton_Clicked(null, null);
+
+            // Reset
+
+            // Assert
+            Assert.IsTrue(true); // Got to here, so it happened...
+        }
 
         [Test]
         public void BattlePage_FocusedAttackButton_Clicked_Default_Should_Pass()
