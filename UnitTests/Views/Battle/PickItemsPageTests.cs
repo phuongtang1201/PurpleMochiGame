22b001bb504c5007867fb3cd5f94f71d7e8d4797
phuongtang1201 <mtang1@seattleu.edu>
2022-03-10 00:14:13 -0800

    Added UT PickItemsPage_NextPage_Clicked_Default in PickItemsPageTests.cs

--- UnitTests/Views/Battle/PickItemsPageTests.cs
+++ UnitTests/Views/Battle/PickItemsPageTests.cs
@@ -58,5 +58,18 @@ namespace UnitTests.Views
             // Assert
             Assert.IsTrue(true); // Got to here, so it happened...
         }
+
+        [Test]
+        public void PickItemsPage_NextPage_Clicked_Default_Should_Pass()
+        {
+            // Arrange
+            // Act
+            page.NextPage_Clicked(null, null);
+
+            // Reset
+
+            // Assert
+            Assert.IsTrue(true); // Got to here, so it happened...
+        }
     }
 }
