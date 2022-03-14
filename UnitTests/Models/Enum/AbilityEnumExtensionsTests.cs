1e778ee17d214949d3e09424325259b68c478223
phuongtang1201 <mtang1@seattleu.edu>
2022-03-14 14:16:33 -0700

    Changed expected message of UT AbilityEnumExtensionsTests_Bandage_Default in AbilityEnumExtensionsTests.cs

--- UnitTests/Models/Enum/AbilityEnumExtensionsTests.cs
+++ UnitTests/Models/Enum/AbilityEnumExtensionsTests.cs
@@ -32,7 +32,7 @@ namespace UnitTests.Models
             // Reset
 
             // Assert
-            Assert.AreEqual("Apply Bandages", result);
+            Assert.AreEqual("takes a break", result);
         }
 
         [Test]
