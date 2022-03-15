9394b0e4c61bb6c07e4f3d3f9a2ee19fac9582f9
Erika <erikasolsen@yahoo.com>
2022-03-14 17:11:00 -0700

    Changed expected result of AbilityEnumHelper_ GetListCleric_Should_Pass in AbilityEnumHelperTests.cs.

--- UnitTests/Helpers/AbilityEnumHelperTests.cs
+++ UnitTests/Helpers/AbilityEnumHelperTests.cs
@@ -45,7 +45,7 @@ namespace UnitTests.Helpers
             var result = AbilityEnumHelper.GetListCleric;
 
             // Assert
-            Assert.AreEqual(8, result.Count);
+            Assert.AreEqual(5, result.Count);
         }
 
         [Test]
